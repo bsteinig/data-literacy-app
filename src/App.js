712562d518622ba8ec05ce6085e83bbeeed854f2
Benjamin Steinig <bsteinig@umich.edu>
2020-10-09 02:52:33 -0400

    Completed mock-up of expanding table of contents

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ function App() {
     <div className="Scalar">
       <body>
         <article>
-            <ul>
-              <li class="dropdown">
+            <ul class="dropbtn">
+              <li class="dropdown"><a href="https://scalar.usc.edu/works/teaching-data-literacy-in-social-studies-an-interactive-professional-development-tool/what-is-data-literacy">
                 <div class="media-left">
                   <img src="https://scalar.usc.edu/works/teaching-data-literacy-in-social-studies-an-interactive-professional-development-tool/media/Download%20Image.png" alt="" class="media-object center-block"/>
                 </div>
+                </a>
                 <div class="content-links">
+                  <a href="https://scalar.usc.edu/works/teaching-data-literacy-in-social-studies-an-interactive-professional-development-tool/what-is-data-literacy">
                   <h4>What is data literacy?</h4>
                   <p>This module provides an introduction to data literacy and its importance for informed and competent citizenship.</p>
+                  </a>
                   <ul class="dropbtn">
                     <li class="dropdown-content"><a href="https://scalar.usc.edu/works/teaching-data-literacy-in-social-studies-an-interactive-professional-development-tool/how-data-literate-are-you">
                       <div class="media-left">
@@ -25,15 +28,77 @@ function App() {
                       </div>
                       </a>
                     </li>
+                    <li class="dropdown-content"><a href="https://scalar.usc.edu/works/teaching-data-literacy-in-social-studies-an-interactive-professional-development-tool/a-closer-look-at-misleading-data-visualizations">
+                        <div class="media-left">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Magnifying_glass_01.svg/241px-Magnifying_glass_01.svg.png" alt="" class="media-object center-block"/>
+                        </div>
+                        <div class="content-links">
+                          <h4>A Closer Look at Misleading Data Visualizations</h4>
+                          <p>There are multiple ways that data visualizations can mislead readers or viewers. This exercise provides a summary of the ways data visualizations can deceive, and provides some examples.</p>
+                        </div>
+                        </a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li class="dropdown">
+                <div class="media-left">
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/2/29/Minard.png" alt="" class="media-object center-block"/>
+                </div>
+                <div class="content-links">
+                  <h4>Why teach data literacy in social studies?</h4>
+                  <p>Not only is data literacy required by state standards and included in standardized assessments, it is important for understanding core social studies disciplines. This module addresses the role that
+                     data visualizations play in social studies standards, assessments, and texts. It also provides links to resources that can be used to support data literacy as a component of disciplinary literacy.</p>
+                  <ul class="dropbtn">
                     <li class="dropdown-content"><a href="https://scalar.usc.edu/works/teaching-data-literacy-in-social-studies-an-interactive-professional-development-tool/how-data-literate-are-you">
                       <div class="media-left">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Online_Survey_Icon_or_logo.svg/240px-Online_Survey_Icon_or_logo.svg.png" alt="" class="media-object center-block"/>
+                        <img src="https://scalar.usc.edu/works/teaching-data-literacy-in-social-studies-an-interactive-professional-development-tool/media/Screenshot%202020-05-15%2017.29.26.png" alt="" class="media-object center-block"/>
                       </div>
                       <div class="content-links">
-                        <h4>How data literate are you?</h4>
+                        <h4>Data Literacy in Social Studies Standards</h4>
                         <p>Answer the questions in this survey about your own sense of data literacy.</p>
                       </div>
                       </a>
+                    </li>
+                    <li class="dropdown-content"><a href="https://scalar.usc.edu/works/teaching-data-literacy-in-social-studies-an-interactive-professional-development-tool/how-data-literate-are-you">
+                        <div class="media-left">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/NatlAssessmentOfEduProgress.jpg" alt="" class="media-object center-block"/>
+                        </div>
+                        <div class="content-links">
+                          <h4>Data Literacy in Standardized Tests</h4>
+                          <p>There are multiple ways that data visualizations can mislead readers or viewers. This exercise provides a summary of the ways data visualizations can deceive, and provides some examples.</p>
+                        </div>
+                        </a>
+                    </li>
+                    <li class="dropdown-content"><a href="https://scalar.usc.edu/works/teaching-data-literacy-in-social-studies-an-interactive-professional-development-tool/how-data-literate-are-you">
+                        <div class="media-left">
+                        <img src="https://tile.loc.gov/storage-services/service/pnp/ppmsca/33800/33870r.jpg" alt="" class="media-object center-block"/>
+                        </div>
+                        <div class="content-links">
+                          <h4>Data Visualizations as Primary Sources</h4>
+                          <p>There are multiple ways that data visualizations can mislead readers or viewers. This exercise provides a summary of the ways data visualizations can deceive, and provides some examples.</p>
+                        </div>
+                        </a>
+                    </li>
+                    <li class="dropdown-content"><a href="https://scalar.usc.edu/works/teaching-data-literacy-in-social-studies-an-interactive-professional-development-tool/how-data-literate-are-you">
+                        <div class="media-left">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/6/63/Textbook.JPG" alt="" class="media-object center-block"/>
+                        </div>
+                        <div class="content-links">
+                          <h4>Analyzing Data Visualizations in Social Studies Textbooks</h4>
+                          <p>There are multiple ways that data visualizations can mislead readers or viewers. This exercise provides a summary of the ways data visualizations can deceive, and provides some examples.</p>
+                        </div>
+                        </a>
+                    </li>
+                    <li class="dropdown-content"><a href="https://scalar.usc.edu/works/teaching-data-literacy-in-social-studies-an-interactive-professional-development-tool/how-data-literate-are-you">
+                        <div class="media-left">
+                        <img src="https://scalar.usc.edu/works/teaching-data-literacy-in-social-studies-an-interactive-professional-development-tool/media/Screenshot%202020-05-18%2007.32.36.png" alt="" class="media-object center-block"/>
+                        </div>
+                        <div class="content-links">
+                          <h4>Finding Data and Data Visualizations Online</h4>
+                          <p>There are multiple ways that data visualizations can mislead readers or viewers. This exercise provides a summary of the ways data visualizations can deceive, and provides some examples.</p>
+                        </div>
+                        </a>
                     </li>
                   </ul>
                 </div>
