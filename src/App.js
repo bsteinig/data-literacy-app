07ead13e1216ae47752a5ccedbd8c323ae14cb57
Benjamin Steinig <bsteinig@umich.edu>
2020-10-09 01:59:57 -0400

    Started Creating condensed table of contents

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Scalar">
+      <body>
+        <article>
+            <ul>
+              <li class="dropdown">
+                <div class="media-left">
+                  <img src="https://scalar.usc.edu/works/teaching-data-literacy-in-social-studies-an-interactive-professional-development-tool/media/Download%20Image.png" alt="" class="media-object center-block"/>
+                </div>
+                <div class="content-links">
+                  <h4>What is data literacy?</h4>
+                  <p>This module provides an introduction to data literacy and its importance for informed and competent citizenship.</p>
+                  <ul class="dropbtn">
+                    <li class="dropdown-content"><a href="https://scalar.usc.edu/works/teaching-data-literacy-in-social-studies-an-interactive-professional-development-tool/how-data-literate-are-you">
+                      <div class="media-left">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Online_Survey_Icon_or_logo.svg/240px-Online_Survey_Icon_or_logo.svg.png" alt="" class="media-object center-block"/>
+                      </div>
+                      <div class="content-links">
+                        <h4>How data literate are you?</h4>
+                        <p>Answer the questions in this survey about your own sense of data literacy.</p>
+                      </div>
+                      </a>
+                    </li>
+                    <li class="dropdown-content"><a href="https://scalar.usc.edu/works/teaching-data-literacy-in-social-studies-an-interactive-professional-development-tool/how-data-literate-are-you">
+                      <div class="media-left">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Online_Survey_Icon_or_logo.svg/240px-Online_Survey_Icon_or_logo.svg.png" alt="" class="media-object center-block"/>
+                      </div>
+                      <div class="content-links">
+                        <h4>How data literate are you?</h4>
+                        <p>Answer the questions in this survey about your own sense of data literacy.</p>
+                      </div>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+        </article>
+      </body>
     </div>
   );
 }
